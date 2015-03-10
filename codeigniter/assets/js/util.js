0fa87762735bacfac2d32e222bdd01bfc3e991d8
nicolas silva <sovietfrance@gmail.com>
2015-03-10 17:28:57 -0300

    add all js into file

--- codeigniter/assets/js/util.js
+++ codeigniter/assets/js/util.js
@@ -1,6 +1,4 @@
-// ********************************************************************************************
-// Quita los espacios del lado derecho de un string
-// ********************************************************************************************
+// remueve right spaces
 function Rtrim(sTmp1) {
     var iTmp1 = 0;
     var oTmp1 = sTmp1;
@@ -17,10 +15,7 @@ function Rtrim(sTmp1) {
     sTmp1 = oTmp1;
     return(sTmp1);
 }
-
-// ********************************************************************************************
-// Quita los espacios del lado izquierdo de un string
-// ********************************************************************************************
+// remove left spaces
 function Ltrim(sTmp1) {
     var iTmp1 = 0;
     var oTmp1 = sTmp1;
@@ -36,10 +31,97 @@ function Ltrim(sTmp1) {
     sTmp1 = oTmp1;
     return(sTmp1);
 }
-
-// ********************************************************************************************
-// Llama a las funciones que quitan todos los espacios de ambos lados de un string
-// ********************************************************************************************
+// remove right - left spaces
 function Trim(sTmp1) {
     return(Rtrim(Ltrim(sTmp1)));
 }
+
+
+//
+$(document).ready(function() {
+
+$("#enviaForm").click(function( event ) {
+    event.preventDefault();
+    
+    /**
+     * refresco contenido modal
+     * @type {String}
+     */
+    var html = '';
+    $("#resultadoBusqueda .modal-body p").html(html);
+
+
+    /**
+     * VALIDA NUMERO TARJETA
+     */
+     var numTarjeta = $("#numTarjetaInput").val();
+     if (Trim(numTarjeta) != "") {
+         numTarjeta = Trim(numTarjeta);
+         var iPrimerCaracterValido = -1;
+         for (var iTmp1 = 0; iTmp1 <= numTarjeta.length - 1; iTmp1++) {
+             if (numTarjeta.substr(iTmp1, 1) != "0") {
+                 iPrimerCaracterValido = iTmp1;
+                 break;
+             }
+         }
+         if (iPrimerCaracterValido >= 0) {
+             numTarjeta = numTarjeta.substring(iPrimerCaracterValido);
+         } else {
+             numTarjeta = "";
+         }
+     }
+     if (Trim(numTarjeta) == "") {
+         $("#numTarjetaInput").val("");
+         alert("Por favor, ingrese el número de su tarjeta bip!");
+         $("#numTarjetaInput").focus();
+         return false;
+     }
+    $("#numTarjetaInput").val(numTarjeta);
+
+    if($('#numTarjetaInput').val() === ''){
+        alert("debes completar el campo");
+    }else{
+        $.ajax({
+            url     : './index.php/buscaestado',
+            type    : 'POST',
+            //dataType: 'json',
+            data    : {'numTarjetaInput' : numTarjeta},
+            dataType: 'json',
+            success : function( data ) {
+
+                      var html   = '<b>Saldo tarjeta bip: </b>'+data.saldoTarjeta+'<br>';
+                          html  += '<b>Fecha saldo: </b>'+data.fechaSaldo+'<br>';
+                          html  += '<b>ID Tarjeta: </b>'+data.idTarjeta+'<br>';
+                          html  += '<b>Estado contrato: </b>'+data.estadoContrato+'<br>';
+                      if(data === 'ID de la tarjeta invalido'){html = 'ID de la tarjeta inválido'};
+                                $("#resultadoBusqueda .modal-body p").html(html);
+                                $('#resultadoBusqueda').modal('show');
+          }, error   : function( xhr, err ) {
+                        console.log(' Error =( ');
+                        var html = data;
+                        $("#resultadoBusqueda .modal-body p").html(json_decode(html));
+                        $('#resultadoBusqueda').modal('show');
+                     }
+           });      
+        }
+    });
+
+    /* activate scrollspy menu */
+    $('body').scrollspy({
+      target: '#navbar-collapsible',
+      offset: 50
+    });
+    /* smooth scrolling sections */
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 50
+            }, 500);
+            return false;
+          }
+        }
+    });        
+});
